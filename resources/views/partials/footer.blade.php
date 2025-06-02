13fd4d1fbf9fb1fdc2719b78e0f8fe6d1a9b7b12
minaashi <syahminamosdy03@gmail.com>
2025-06-02 08:32:05 +0800

    update footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,3 @@
 <footer class="bg-blue-800 text-white p-4">
-    <p class="text-center text-sm">Copyright © 2025 Nursyahmina Mosdy, Dr Cik.Feresa Mohd Foozy</p>
+    <p class="text-center text-sm">Copyright © 2025 Nursyahmina Mosdy</p>
 </footer> 
