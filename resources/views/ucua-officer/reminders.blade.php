74cec98540893ac09e6b59682006b4c2392aac5e
minaashi <syahminamosdy03@gmail.com>
2025-06-02 08:02:03 +0800

    delete "send reminder"button

--- resources/views/ucua-officer/reminders.blade.php
+++ resources/views/ucua-officer/reminders.blade.php
@@ -10,9 +10,7 @@
                 </h1>
                 <p class="text-red-900 text-base">Send and track reminders for urgent and overdue reports.</p>
             </div>
-            <div>
-                <button onclick="sendReminder()" class="bg-red-700 hover:bg-red-800 text-white font-semibold px-4 py-2 rounded shadow transition">+ Send New Reminder</button>
-            </div>
+           
         </div>
         <div class="bg-white rounded-b-lg shadow-md p-8 mt-2">
             <!-- Stats Cards -->
